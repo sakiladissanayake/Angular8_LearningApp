8806d924eee1bb5ceadeeba5ea74a6d21db133a0
sakiladissanayake <sakilad@gmail.com>
2020-03-21 21:03:06 +0530

    show recipes in recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is tet', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'),
+    new Recipe('Test Recipe', 'This is tet', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,9 +1,9 @@
-export class RecipeModel {
+export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
 
-    constructor(name: string, des: string, imagePath: string){
+    constructor(name: string, des: string, imagePath: string) {
         this.name = name;
         this.description = des;
         this.imagePath = imagePath;
